93ea41b1455803ef085854d65ece2ece483c52c3
Никита Савцов <nsavtsov89@inbox.ru>
2024-11-24 04:04:57 +0500

    Task2

--- Tyuiu.SavtsovNE.Sprint5.Task2.V4.Test/DataServiceTest.cs
+++ Tyuiu.SavtsovNE.Sprint5.Task2.V4.Test/DataServiceTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Diagnostics;
 using System.IO;
 using Tyuiu.SavtsovNE.Sprint5.Task2.V4.Lib;
 
@@ -11,8 +12,9 @@ namespace Tyuiu.SavtsovNE.Sprint5.Task2.V4.Test
         [TestMethod]
         public void CheckedExistsFile()
         {
-            string path = @"C:\Users\1\source\repos\Tyuiu.ModenovaAP.Sprint5\Tyuiu.ModenovaAP.Sprint5.Task2.V4\bin\Debug\OutPutFileTask2.csv";
-            FileInfo fileInfo = new FileInfo(path);
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.SavtsovNE.Sprint5\Tyuiu.SavtsovNE.Sprint5.Task2.V4\bin\Debug\net8.0\OutPutFileTask2.csv";
+                            
+        FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
             Assert.AreEqual(wait, fileExists);
