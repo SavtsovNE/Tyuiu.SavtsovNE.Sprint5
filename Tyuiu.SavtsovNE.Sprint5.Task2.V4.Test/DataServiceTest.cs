3c11b92532bbfe0637b4ae424bf90cdfb0f672a3
Никита Савцов <nsavtsov89@inbox.ru>
2024-11-24 04:12:57 +0500

    Task2

--- Tyuiu.SavtsovNE.Sprint5.Task2.V4.Test/DataServiceTest.cs
+++ Tyuiu.SavtsovNE.Sprint5.Task2.V4.Test/DataServiceTest.cs
@@ -12,9 +12,9 @@ namespace Tyuiu.SavtsovNE.Sprint5.Task2.V4.Test
         [TestMethod]
         public void CheckedExistsFile()
         {
-            string path = @"C:\Users\Admin\source\repos\Tyuiu.SavtsovNE.Sprint5\Tyuiu.SavtsovNE.Sprint5.Task2.V4\bin\Debug\net8.0\OutPutFileTask2.csv";
-                            
-            FileInfo fileInfo = new FileInfo(path);
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.SavtsovNE.Sprint5\Tyuiu.SavtsovNE.Sprint5.Task2.V4\bin\Debug\OutPutFileTask2.csv";
+                           
+        FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
             Assert.AreEqual(wait, fileExists);
