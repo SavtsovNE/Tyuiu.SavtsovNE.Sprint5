273a289fbafc1e4b5248ebc3eeea8248102f0613
Никита Савцов <nsavtsov89@inbox.ru>
2024-11-26 00:11:07 +0500

    Task7

---
+++ Tyuiu.SavtsovNE.Sprint5.Task7.V5.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.SavtsovNE.Sprint5.Task7.V5.Lib
+{
+    public class DataService : ISprint5Task7V5
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string strLine = "";
+            using (StreamReader R = new(path))
+            {
+                string L;
+                while ((L = R.ReadLine()) != null)
+                {
+                    for (int i = 0; i < L.Length; i++)
+                    {
+                        if ((L[i] >= 'А' && L[i] <= 'я' || L[i] == '!' || L[i] == '.' || L[i] == ',') ^ (L[i] == ' '))
+                        {
+                            strLine = strLine + L[i];
+                        }
+                    }
+                }
+                return strLine.Trim('!');
+            }
+        }
+    }
+}
---
+++ Tyuiu.SavtsovNE.Sprint5.Task7.V5/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.SavtsovNE.Sprint5.Task7.V5.Lib;
+namespace Tyuiu.SavtsovNE.Sprint5.Task7.V5
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Савцов Никита Евгеньевич | ИБКСб-24-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спритн #5                                                              *");
+            Console.WriteLine("* Тема: Создания итогого решения по спринту                              *");
+            Console.WriteLine("* Задание #7                                                             *");
+            Console.WriteLine("* Вариант #5                                                             *");
+            Console.WriteLine("* Выполнил: Савцов Никита Евгеньевич | ИБКСб-24-1                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            DataService ds = new DataService();
+            string path = @"";
+            Console.WriteLine($"Данные находятся в файле: {path}");
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Находится в файле:");
+            Console.WriteLine(ds.LoadDataAndSave(path));
+            Console.ReadKey();
+        }
+    }
+}
