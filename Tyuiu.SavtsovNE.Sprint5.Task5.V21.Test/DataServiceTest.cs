9455ad83704ed422d6d991b5b9af0c829ce43ed4
Никита Савцов <nsavtsov89@inbox.ru>
2024-11-25 22:05:29 +0500

    Task5

--- Tyuiu.SavtsovNE.Sprint5.Task5.V21.Test/DataServiceTest.cs
+++ Tyuiu.SavtsovNE.Sprint5.Task5.V21.Test/DataServiceTest.cs
@@ -1,14 +1,18 @@
-namespace Tyuiu.SavtsovNE.Sprint5.Task5.V21.Test
+public class DataService
 {
-    [TestClass]
-    public class DataServiceTest
+    public double DoubleFactorial(int number)
     {
-        [TestMethod]
-        public void TestMethod1()
+        if (number < 0)
         {
-           
-            //Assert.AreEqual(120, DataServiceTest.Factorial(5));
-            //Assert.AreEqual(1, DataServiceTest.Factorial(0));
+            throw new ArgumentException("Negative numbers are not supported.");
         }
+
+        if (number == 0 || number == 1)
+        {
+            return 1;
+        }
+
+        return number * DoubleFactorial(number - 2);
     }
 }
+
